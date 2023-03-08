d4c1495e0c47eb975db5ec0803d4b6000c5e4b0a
Cristian Ceballos <cristian.ceballos@precompro.com>
2023-03-08 03:37:43 +0100

    Manual Error handling and first part of flash smorest with abort

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -13,6 +14,11 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure name field is present.")
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists: {store['name']}")
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -26,8 +32,24 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request, Ensure all required fields are present"
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists: {item['name']}")
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
+        # return {"message": "Store not found"}, 404
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
@@ -50,7 +72,8 @@ def get_specific_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
+        # return {"message": "Store not found"}, 404
     # store = next((store for store in stores if store["name"] == name), None)
     # if store == None:
     #     return {"message": "Store not found"}, 404
@@ -62,7 +85,8 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found.")
+        # return {"message": "Item not found"}, 404
     # store = next((store for store in stores if store["name"] == name), None)
     # if store == None:
     #     return {"message": "Store not found"}, 404
