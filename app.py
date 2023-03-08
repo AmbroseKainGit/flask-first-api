ee2ade69a203e0c51f986bc6a681acf0801f8635
Cristian Ceballos <cristian.ceballos@precompro.com>
2023-03-08 03:16:17 +0100

    Add requirements.txt file which is used to describe the libraries used in the project create the .flaskenv file to create our enviroments variables General changes to the api

--- app.py
+++ app.py
@@ -1,55 +1,69 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
 
 
-stores = [
-    {
-        "name": "My Store", "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
-
-
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"data": list(stores.values())}
 
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+    # request_data = request.get_json()
+    # new_store = {"name": request_data["name"], "items": []}
+    # stores.append(new_store)
+    # return new_store, 201
 
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    store = next((store for store in stores if store["name"] == name), None)
-    if store == None:
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
         return {"message": "Store not found"}, 404
-    new_item = {"name": request_data["name"], "price": request_data["price"]}
-    store["items"].append(new_item)
-    return new_item, 201
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+    return item, 201
+    # store = next((store for store in stores if store["name"] == name), None)
+    # if store == None:
+    #     return {"message": "Store not found"}, 404
+    # new_item = {"name": request_data["name"], "price": request_data["price"]}
+    # store["items"].append(new_item)
+    # return new_item, 201
 
 
-@app.get("/store/<string:name>")
-def get_specific_store(name):
-    store = next((store for store in stores if store["name"] == name), None)
-    if store == None:
+@app.get("/item")
+def get_all_items():
+    return {"data": list(items.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_specific_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
         return {"message": "Store not found"}, 404
-    return {"data": store}
+    # store = next((store for store in stores if store["name"] == name), None)
+    # if store == None:
+    #     return {"message": "Store not found"}, 404
+    # return {"data": store}
 
 
-@app.get("/store/<string:name>/item")
-def get_store_items(name):
-    store = next((store for store in stores if store["name"] == name), None)
-    if store == None:
-        return {"message": "Store not found"}, 404
-    return {"items": store["items"]}
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
+    # store = next((store for store in stores if store["name"] == name), None)
+    # if store == None:
+    #     return {"message": "Store not found"}, 404
+    # return {"items": store["items"]}
