eec9662f8a8e714f38a902a6b310b0e7f3485e14
Cristian Ceballos <cristian.ceballos@precompro.com>
2023-03-10 18:11:04 +0100

    Install SQLAlchemy and flask-sqlalchemy for db management Create models for store and items instead of plain python dictionaries change app.py to use a function to create the flask app instead of creating it in the file itselt change the Schemas to be PlainSchemas for the default Fields and OPther schemas for the database relationship fields Change the endpoing code to use DatabaseModels and database queries for all the dataManagement

--- resources/item.py
+++ resources/item.py
@@ -1,9 +1,9 @@
-import uuid
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
 from schemas import ItemSchema, ItemUpdateSchema, ResponseSchema
-
+from models import ItemModel
+from db import db
+from sqlalchemy.exc import SQLAlchemyError
 blp = Blueprint("Items", __name__, description="Operations on items")
 
 
@@ -12,26 +12,39 @@ class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
-            return items[item_id]
+            # item = ItemModel.query.get_or_404(item_id)
+            return ItemModel.query.get_or_404(item_id)
         except KeyError:
             abort(404, message="Item not found.")
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted"}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted"}
+        # raise NotImplementedError("Deleting an item is not implemented.")
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ResponseSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
-            item |= item_data
-            return {"message": "Item updated", "data": item}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get(item_id)
+        message = "" 
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+            message = "Item updated"
+        else:
+            item = ItemModel(id=item_id, **item_data)
+            message = "Item created"
+        db.session.add(item)
+        db.session.commit()
+        return {"message": message, "data": item}       
+        # try:
+        #     item = items[item_id]
+        #     item |= item_data
+        #     return {"message": "Item updated", "data": item}
+        # except KeyError:
+        #     abort(404, message="Item not found.")
 
 
 @blp.route("/item")
@@ -39,21 +52,29 @@ class ItemList(MethodView):
     @blp.response(200, ItemSchema(many=True))
     # @blp.response(200, ResponseSchema)
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
+        # return items.values()
         # return {"message": "Items", "data": list(items.values())}
 
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists: {item['name']}")
+        item = ItemModel(**item_data)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            db.session.rollback()
+            abort(500, message="An Error ocurred while inserting the item.")
+        return item, 201
+        # for item in items.values():
+        #     if (
+        #         item_data["name"] == item["name"]
+        #         and item_data["store_id"] == item["store_id"]
+        #     ):
+        #         abort(400, message=f"Item already exists: {item['name']}")
         # if item_data["store_id"] not in stores:
         #     abort(404, message="Store not found.")
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
-        return item, 201
+        # item_id = uuid.uuid4().hex
+        # item = {**item_data, "id": item_id}
+        # items[item_id] = item
--- resources/store.py
+++ resources/store.py
@@ -1,10 +1,9 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
 from schemas import StoreSchema
-
+from models import StoreModel
+from db import db
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 blp = Blueprint("Stores", "store", description="Operations on stores")
 
 
@@ -12,32 +11,48 @@ blp = Blueprint("Stores", "store", description="Operations on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        # store = StoreModel.query.get_or_404(store_id)
+        return StoreModel.query.get_or_404(store_id)
+        # try:
+        #     return stores[store_id]
+        # except KeyError:
+        #     abort(404, message="Store not found.")
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted"}
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}
+        # try:
+        #     del stores[store_id]
+        #     return {"message": "Store deleted"}
+        # except KeyError:
+        #     abort(404, message="Store not found.")
 
 
 @blp.route("/store", methods=['GET', 'POST'])
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
 
-    @blp.arguments(StoreSchema) 
-    @blp.response(201, StoreSchema)   
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
     def post(self, store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message=f"Store already exists: {store['name']}")
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            abort(400, message="A store with that name already exists.")
+        except SQLAlchemyError:
+            db.session.rollback()
+            abort(500, message="An Error ocurred while creating the store.")
+        # for store in stores.values():
+        #     if store_data["name"] == store["name"]:
+        #         abort(400, message=f"Store already exists: {store['name']}")
+        # store_id = uuid.uuid4().hex
+        # store = {**store_data, "id": store_id}
+        # stores[store_id] = store
         return store, 201
