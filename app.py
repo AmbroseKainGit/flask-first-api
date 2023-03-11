92375919c2a0fcea79a10ec014a47135b8478e71
Cristian Ceballos <cmceballos88@gmail.com>
2023-03-11 16:42:36 +0100

    Add jwt initial configuration and package

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
-
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 from db import db
 import models
 from resources.item import blp as ItemBlueprint
@@ -24,6 +24,9 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     api = Api(app)
+    app.config["JWT_SECRET_KEY"] = "248955838486057085944623901479057552838"
+    
+    jwt = JWTManager(app)
     with app.app_context():
         db.create_all()
 
