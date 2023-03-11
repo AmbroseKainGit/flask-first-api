aa867b7b0eadb9fd650acc1f6993f189f277086c
Cristian Ceballos <cristian.ceballos@precompro.com>
2023-03-11 16:18:02 +0100

    Add ItemsTags model to create a secundary table with a many to many relationship to link items and tags together add new endpoints to link and remove linkts from items/tags and to delete tags update schemas to fill tags or items as needed

--- resources/tag.py
+++ resources/tag.py
@@ -2,8 +2,8 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 from db import db
-from models import TagModel, StoreModel
-from schemas import TagSchema
+from models import TagModel, StoreModel, ItemModel
+from schemas import TagSchema, TagAndItemSchema
 blp = Blueprint("tags", __name__, description="Operations on Tags")
 
 
@@ -35,9 +35,63 @@ class Tag(MethodView):
         tag = TagModel.query.get_or_404(tag_id)
         return tag
 
+    @blp.response(
+        202,
+        description="Deletes a tag if no item is associated with it",
+        example={"message": "Tag deleted"}
+    )
+    @blp.alt_response(404, description="Tag not found")
+    @blp.alt_response(
+        400,
+        description="Returned if the tag is assigned to one or more items. In this case, the tag is not deleted"
+    )
+    def delete(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+        if not tag.items:
+            db.session.delete(tag)
+            db.session.commit()
+            return {"message": "Tag deleted"}
+        abort(
+            400,
+            message="Could not delete tag. Make sure tag is not associated with any items, then try again"
+        )
+
 
 @blp.route("/tag")
 class TagAll(MethodView):
     @blp.response(200, TagSchema(many=True))
     def get(self):
         return TagModel.query.all()
+
+
+@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+class LinkTagsToItem(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+        if tag.store_id != item.store_id:
+            abort(400, message="Tag and item must belong to the same store")
+        item.tags.append(tag)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="Failed to link tags to item")
+
+        return tag
+
+    @blp.response(201, TagAndItemSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+        if tag.store_id != item.store_id:
+            abort(400, message="Tag and item must belong to the same store")
+        item.tags.remove(tag)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="Failed to link tags to item")
+
+        return {"message": "Tag successfully removed from item", "tag": tag, "item": item}
